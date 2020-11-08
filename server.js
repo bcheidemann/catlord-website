2ce8ae68a1b125b73c350232d7373bc496cc91c2
Ben Heidemann <ben@heidemann.co.uk>
2020-11-08 11:20:35 +0000

    fix: server serves files when accessing /files/*

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const fs = require('fs')
 
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/files/*', function (req, res) {
+    var filePath = path.join(__dirname, req.url);
+    fs.access(filePath, fs.F_OK, (err) => {
+        if (err) {
+            res.redirect('/404');
+            return;
+        }
+        res.sendFile(filePath);
+    });
+});
+
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
